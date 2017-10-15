f04c08a040a4c2c3a1641b0ed0f5d25ba51b25c0
briany2 <32102038+briany2@users.noreply.github.com>
2017-10-15 14:20:29 -0500

    Movement
    Movement

---
+++ Hack-and-Slash/Assets/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    private GameObject wayPoint;
+    private Vector3 wayPointPos;
+    private float speed = 2.5f;
+    // Finds the player object
+	void Start () {
+        wayPoint = GameObject.Find("some_kind_of_witch_thing");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Takes the position and moves enemy character.
+        Vector3 localPosition = wayPoint.transform.position - transform.position;
+        localPosition = localPosition.normalized;
+        transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
+    }
+}
