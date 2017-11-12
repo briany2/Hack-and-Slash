e92b49a2d30ba9fd3324f7a980bf26fe6d8da590
briany2 <32102038+briany2@users.noreply.github.com>
2017-11-12 14:36:32 -0600

    Github being weird

--- Hack-and-Slash/Assets/Scripts/PlayerHealth.cs
+++ Hack-and-Slash/Assets/Scripts/PlayerHealth.cs
@@ -3,48 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHealth : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour
+{
 
-	//Health system based on classic HP bar
-	//Written by Cheng Hang
-	public int startingHealth = 100;
-	public int currentHealth;
-	public Slider HealthSlider;
-	public Image DamageImage;
-	public float flashSpeed = 5f;
-	public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+    //Health system based on classic HP bar
+    //Written by Cheng Hang
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider HealthSlider;
+    public Image DamageImage;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
 
-	Movement playerMovement;
-	bool isDead;
-	bool isDamaged;
+    Animator playerAnimator;
+    int damagehash;
 
-	// Use this for initialization
-	void Start () {
-		playerMovement = GetComponent<Movement> ();
-		currentHealth = startingHealth;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (isDamaged) {
-			DamageImage.color = flashColor;
-		} else {
-			DamageImage.color = Color.Lerp (DamageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-		}
-		isDamaged = false;
-	}
+    Movement playerMovement;
+    bool isDead;
+    bool isDamaged;
 
-	public void TakeDamage (int amount) {
-		isDamaged = true;
-		currentHealth -= amount;
-		HealthSlider.value = currentHealth;
-		if (currentHealth <= 0 && !isDead) {
-			Death ();
-		}
-	}
 
-	void Death () {
-		isDead = true;
-		playerMovement.enabled = false;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        playerMovement = GetComponent<Movement>();
+        playerAnimator = GetComponent<Animator>();
+
+        damagehash = Animator.StringToHash("Damage");
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDamaged)
+        {
+            DamageImage.color = flashColor;
+            playerAnimator.SetTrigger(damagehash);
+        }
+        else
+        {
+            DamageImage.color = Color.Lerp(DamageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        isDamaged = false;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        isDamaged = true;
+        currentHealth -= amount;
+        HealthSlider.value = currentHealth;
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        playerMovement.enabled = false;
+    }
 }
