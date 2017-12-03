10d6b26c233972fd51b284d8df5524deaddc854f
briany2 <32102038+briany2@users.noreply.github.com>
2017-12-03 16:07:49 -0600

    Added Game Over Screen and Animations.

---
+++ Hack-and-Slash/Assets/Scripts/GameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public PlayerHealth playerHealth;
+    public float restartDelay = 5f;
+
+    Animator anim;
+    float restartTimer;
+
+	void Awake () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+            restartTimer += Time.deltaTime;
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+	}
+}
