fbca918c71e05981e87d49f02ee0835f88e0a541
unknown <chenghang8888@gmail.com>
2017-12-10 16:22:47 -0600

    Finished eyeball enemy.

--- Hack-and-Slash/Assets/Scripts/DamageEnemy.cs
+++ Hack-and-Slash/Assets/Scripts/DamageEnemy.cs
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-// This class allows an enemy to be damaged
-// Created by Brian Yu
-public class DamageEnemy : MonoBehaviour {
-
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider.gameObject.tag == "Enemy")
-        {
-            Destroy(collider.gameObject);
-            Destroy(gameObject);
-            ScoreManager.score++;
-        }
-    }
-}
-=======
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -44,4 +23,3 @@ public class DamageEnemy : MonoBehaviour {
         }
     }
 }
->>>>>>> b1b7578dd45b373b60561e74102a0e834ebf5a35
