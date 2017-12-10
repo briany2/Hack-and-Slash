bf0249c843db9310d917b88dac6f08563614c4ac
briany2 <32102038+briany2@users.noreply.github.com>
2017-12-10 17:37:25 -0600

    Test

--- Hack-and-Slash/Assets/Scripts/DamageEnemy.cs
+++ Hack-and-Slash/Assets/Scripts/DamageEnemy.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DamageEnemy : MonoBehaviour {
-<<<<<<< HEAD
     public AudioClip damageSound;
     private AudioSource source;
     Animator playerAnimator;
-=======
-	// Use this for initialization
-	void Start () {
-        
-    }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
->>>>>>> eyeballEnemy
-
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Enemy")
-<<<<<<< HEAD
         {
             source = GetComponent<AudioSource>();
             source.PlayOneShot(damageSound,0.5f);
@@ -32,11 +18,8 @@ public class DamageEnemy : MonoBehaviour {
             gameObject.transform.position = new Vector2(99999,99999);
             Destroy(gameObject, damageSound.length);
             ScoreManager.score++;
-=======
-		{
 			Destroy (gameObject);
 			collider.gameObject.SendMessage ("TakeDamage", 25);
->>>>>>> eyeballEnemy
         }
     }
 }
