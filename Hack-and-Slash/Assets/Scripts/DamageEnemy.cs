afce1a726ff019db5317b0844759ad24275a4bbf
briany2 <32102038+briany2@users.noreply.github.com>
2017-12-10 17:22:48 -0600

    Test

--- Hack-and-Slash/Assets/Scripts/DamageEnemy.cs
+++ Hack-and-Slash/Assets/Scripts/DamageEnemy.cs
@@ -8,15 +8,18 @@ using UnityEngine.UI;
 public class DamageEnemy : MonoBehaviour {
     public AudioClip damageSound;
     private AudioSource source;
+    Animator playerAnimator;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Enemy")
         {
             source = GetComponent<AudioSource>();
-            source.PlayOneShot(damageSound,1);
+            source.PlayOneShot(damageSound,0.5f);
+            playerAnimator.Play("FrogDeath");
             Destroy(collider.gameObject);
-            Destroy(gameObject);
+            gameObject.transform.position = new Vector2(99999,99999);
+            Destroy(gameObject, damageSound.length);
             ScoreManager.score++;
         }
     }
--- Hack-and-Slash/Assets/Scripts/EnemyMovement.cs
+++ Hack-and-Slash/Assets/Scripts/EnemyMovement.cs
@@ -16,6 +16,8 @@ public class EnemyMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         // Takes the position and moves enemy character.
+        float speedMultiplier = ScoreManager.score / 50;
+        speed += speedMultiplier;
         Vector3 localPosition = wayPoint.transform.position - transform.position;
         localPosition = localPosition.normalized;
         transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
