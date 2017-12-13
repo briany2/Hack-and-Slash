d629b7f046310b7b2ab104f0364cd81d7e3e458d
briany2 <32102038+briany2@users.noreply.github.com>
2017-12-12 21:27:49 -0600

    Finished Project

--- Hack-and-Slash/Assets/Scripts/DamageEnemy.cs
+++ Hack-and-Slash/Assets/Scripts/DamageEnemy.cs
@@ -2,24 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-<<<<<<< HEAD
-public class DamageEnemy : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-        
-    }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
-
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider.gameObject.tag == "Enemy")
-		{
-=======
 // This code is used to damage the enemy and play a sound on hit.
 // Created by: Brian Yu
 public class DamageEnemy : MonoBehaviour {
@@ -29,7 +11,6 @@ public class DamageEnemy : MonoBehaviour {
     {
         if (collider.gameObject.tag == "Enemy")
         {
->>>>>>> GameOver
 			Destroy (gameObject);
 			collider.gameObject.SendMessage ("TakeDamage", 25);
         }
